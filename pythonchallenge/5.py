7c20a61cfbc4b612f4531232fe494661d37caed3
Derrick <derrickkueh1995@gmail.com>
2022-11-08 08:04:09 +0800

    WIP - python challenge 5

--- pythonchallenge/5.py
+++ pythonchallenge/5.py
@@ -1,4 +1,25 @@
-#5 peak is a value bigger than neighbor
-# peak hell sounds familiar?? found banner.p
-import urllib.request
-import re
+#5 
+# assume something to do with peak - a value bigger than neighbor
+# peak hell sounds familiar?? found banner.p - turns out its 'pickle'
+
+import requests
+import pickle
+import io
+
+response = requests.get('http://www.pythonchallenge.com/pc/def/banner.p')
+
+with open("response.txt", "w") as f:
+    f.write(response.text)
+f.close()
+
+infile = open("response.txt",'rb')
+unload = pickle.load(infile)
+infile.close()
+
+row = ""
+for i in unload:
+    for sec in i:
+        row += sec[0] * sec[1]
+
+    print(row)
+    row = ""
